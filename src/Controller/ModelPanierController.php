856dc740327d84c80183ace76076dbd96d9e96a6
studioairi67@gmail.com <studioairi67@gmail.com>
2022-11-12 14:39:48 +0100

    maj

--- src/Controller/ModelPanierController.php
+++ src/Controller/ModelPanierController.php
@@ -20,7 +20,6 @@ class ModelPanierController extends AbstractController
     #[Route('/add', name: 'app_model_panier_add', methods: ['GET'])]
     public function model_panier_add(ModeleRepository $modeles, Request $request): Response
     {
-        //unset($_COOKIE['panier']);
 
         $id = $request->query->get("id"); //récupérer l'id du model
         $model = $modeles->find($id); //récupérer le model de la page
@@ -50,53 +49,133 @@ class ModelPanierController extends AbstractController
             $memoire = [$id, ...$nombres];
             $panier = [...$panier, $memoire];
             setcookie('panier', json_encode($panier));
+            //
+            return $this->render('model/model_panier.html.twig', [
+            'panier' => json_decode($_COOKIE['panier'], true),
+            'modeles' => $modeles->findAll(),
+            'reload' => 1,
+            ]);
         }
         else 
         {
             $memoire = [$id, ...$nombres];
             $panier = [$memoire];
             setcookie('panier', json_encode($panier));
-        }
-        
-        return $this->render('model/model_panier.html.twig', [
-            'panier' => json_decode($_COOKIE['panier'], true),
+            return $this->render('model/model_panier.html.twig', [
+            'panier' => [],
             'modeles' => $modeles->findAll(),
             'reload' => 1,
+            ]);
+        }
+    }
+    #[Route('/deleteAll', name: 'app_model_panier_deleteAll', methods: ['GET'])]
+    public function model_panier_deleteAll(ModeleRepository $modeles): Response
+    {
+        setcookie('panier', null);
+        if (isset($_COOKIE['panier'])) 
+        {
+            return $this->render('model/model_panier.html.twig', [
+                'panier' => json_decode($_COOKIE['panier'], true),
+                'modeles' => $modeles->findAll(),
+                'reload' => 1,
+            ]);
+        }
+        else 
+        {
+            return $this->render('model/model_panier.html.twig', [
+                'panier' => [],
+                'modeles' => $modeles->findAll(),
+                'reload' => 1,
+            ]);
+        }
+    }
+    #[Route('/delete', name: 'app_model_panier_delete', methods: ['GET'])]
+    public function model_panier_delete(ModeleRepository $modeles, Request $request): Response
+    {
+        $article = $request->query->all("article"); 
+        $idModel = $article[0];
+        $idVariante = $request->query->get("idVariante");
+        $nombre = $request->query->get("nombre");
+        $model = $modeles->find($idModel);
+
+        $panier = json_decode($_COOKIE['panier'], true);
+        $newPanier = [];
+
+        for ($i=0; $i<count($panier); $i++)
+        {
+            if ($panier[$i][0] == $idModel){
+                $nb = count($panier[$i]);
+                for ($j=1; $j<$nb; $j++)
+                {
+                    if ($panier[$i][$j][0] == $idVariante && $panier[$i][$j][1] == $nombre)
+                    {
+                        //
+                        if (count($panier[$i]) <= 2)
+                        {
+                            for ($newI=0; $newI<count($panier); $newI++)
+                            {
+                                if ($newI != $i)
+                                {
+                                    $newPanier = [...$newPanier, $panier[$newI]];
+                                }
+                            }
+                        }
+                        else 
+                        {
+                            for ($newI=0; $newI<count($panier); $newI++)
+                            {
+                                if ($newI == $i)
+                                {
+                                    $article = [$idModel];
+                                    for ($newJ=1; $newJ<count($panier[$i]); $newJ++)
+                                    {
+                                        if ($newJ != $j)
+                                        {
+                                            $article = [...$article, $panier[$i][$newJ]];
+                                        }
+                                    }
+                                    $newPanier = [...$newPanier, $article];
+                                }
+                                else
+                                {
+                                    $newPanier = [...$newPanier, $panier[$newI]];
+                                }
+                            }
+                        }
+                        $j = count($panier[$i]);
+                        $i = count($panier);
+                        //
+                    }
+                }
+            }
+        }
+        setcookie('panier', json_encode($newPanier));
+        return $this->render('model/model_panier.html.twig', [
+                'panier' => json_decode($_COOKIE['panier'], true),
+                'modeles' => $modeles->findAll(),
+                'reload' => 1,
         ]);
     }
-
     #[Route('/', name: 'app_model_panier', methods: ['GET'])]
     public function model_panier(ModeleRepository $modeles): Response
     {
-        /*
-        $id = 1;
-        $nombres = [[1,2],[3,4]];
-        $memoire = [$id, ...$nombres];
-        $panier = [$memoire];
-        setcookie('panier', json_encode($panier));
-        */
-
-
-        //unset($_COOKIE['panier']);
-        return $this->render('model/model_panier.html.twig', [
-            'panier' => json_decode($_COOKIE['panier'], true),
-            'modeles' => $modeles->findAll(),
-            'reload' => 0,
-        ]);
-
-
+        //setcookie('panier', null);
+        if (isset($_COOKIE['panier'])) 
+        {
+            return $this->render('model/model_panier.html.twig', [
+                'panier' => json_decode($_COOKIE['panier'], true),
+                'modeles' => $modeles->findAll(),
+                'reload' => 0,
+            ]);
+        }
+        else 
+        {
+            return $this->render('model/model_panier.html.twig', [
+                'panier' => [],
+                'modeles' => $modeles->findAll(),
+                'reload' => 0,
+            ]);
+        }
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-   
+} 
  ?>
--- var/cache/dev/url_matching_routes.php
+++ var/cache/dev/url_matching_routes.php
@@ -26,6 +26,8 @@ return [
         '/' => [[['_route' => 'app_main', '_controller' => 'App\\Controller\\MainController::main'], null, ['GET' => 0], null, false, false, null]],
         '/search' => [[['_route' => 'app_search_page', '_controller' => 'App\\Controller\\MainController::search_page'], null, ['GET' => 0], null, false, false, null]],
         '/panier/add' => [[['_route' => 'app_model_panier_add', '_controller' => 'App\\Controller\\ModelPanierController::model_panier_add'], null, ['GET' => 0], null, false, false, null]],
+        '/panier/deleteAll' => [[['_route' => 'app_model_panier_deleteAll', '_controller' => 'App\\Controller\\ModelPanierController::model_panier_deleteAll'], null, ['GET' => 0], null, false, false, null]],
+        '/panier/delete' => [[['_route' => 'app_model_panier_delete', '_controller' => 'App\\Controller\\ModelPanierController::model_panier_delete'], null, ['GET' => 0], null, false, false, null]],
         '/panier' => [[['_route' => 'app_model_panier', '_controller' => 'App\\Controller\\ModelPanierController::model_panier'], null, ['GET' => 0], null, true, false, null]],
         '/modele' => [[['_route' => 'app_modele_index', '_controller' => 'App\\Controller\\ModeleController::index'], null, ['GET' => 0], null, true, false, null]],
         '/modele/new' => [[['_route' => 'app_modele_new', '_controller' => 'App\\Controller\\ModeleController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
